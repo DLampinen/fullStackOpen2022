5f8d4e6197b246cee3290f87deb127c35bbdb2b1
Dick Lampinen <dlampine@gmail.com>
2022-03-16 23:50:19 +0200

    Finished ex 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,14 +1,11 @@
-// eslint-disable-next-line
 const Header = ({ course }) => <h1>{course}</h1>;
 
-// eslint-disable-next-line
 const Part = ({ part, exercises }) => (
   <p>
     {part} {exercises}
   </p>
 );
 
-/* eslint-disable */
 const Content = ({ parts }) => (
   <>
     <Part part={parts[0].name} exercises={parts[0].exercises} />
@@ -17,34 +14,37 @@ const Content = ({ parts }) => (
   </>
 );
 
-
 const Total = ({ parts }) => (
-  <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises} </p>
+  <p>
+    Number of exercises{' '}
+    {parts[0].exercises + parts[1].exercises + parts[2].exercises}{' '}
+  </p>
 );
-/* eslint-enable */
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
