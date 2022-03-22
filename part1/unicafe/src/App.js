ac798b899ce4fedcf85a2fdf228b403b92b06ffe
Dick Lampinen <dlampine@gmail.com>
2022-03-22 16:00:36 +0200

    Added button-component according to ex 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Button from './Button';
 
 const App = () => {
   // save clicks of each button to its own state
@@ -6,7 +7,14 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button text="good" />
+      <Button text="neutral" />
+      <Button text="bad" />
+    </div>
+  );
 };
 
 export default App;
