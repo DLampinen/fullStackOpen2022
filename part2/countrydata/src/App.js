ae32d7ecbe46ceed5da368e36ae39136a8c1635c
Dick Lampinen <dlampine@gmail.com>
2022-04-10 00:00:51 +0300

    Started working on the country data exercise

--- part2/countrydata/src/App.js
+++ part2/countrydata/src/App.js
@@ -1,11 +1,21 @@
+import { useState, useEffect } from 'react';
 import axios from 'axios';
+import SearchCountry from './SearchCountry';
 
-function App() {
-  axios
-    .get('https://restcountries.com/v2/all')
-    .then((response) => console.log(response.data));
+const App = () => {
+  const [countries, setCountries] = useState([]);
 
-  return <div>Lalaallalalal</div>;
-}
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all').then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
+  countries.map((country) => console.log(country.name.common));
+  return (
+    <div>
+      <SearchCountry text="find countries" />
+    </div>
+  );
+};
 
 export default App;
---
+++ part2/countrydata/src/SearchCountry.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line
+const SearchCountry = ({ text, value, onChange }) => (
+  <>
+    {text} <input value={value} onChange={onChange} />
+  </>
+);
+
+export default SearchCountry;
